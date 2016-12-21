cf25b1df137212a94bff397a39433750cf756c9d
James Aimonetti <james.aimonetti@gmail.com>
2016-12-21 19:13:34 +0000

    fix clause

--- applications/tasks/src/modules/kt_rates.erl
+++ applications/tasks/src/modules/kt_rates.erl
@@ -178,7 +178,7 @@ import(Props, State, ?VARS) ->
                       Db = props:get_value('db', State),
                       save_rates(Db, [JObj | Docs]),
                       [];
-                  'false' -> [JObj | Docs]
+                  _Rem -> [JObj | Docs]
               end,
     {[], props:set_values([{'count', Count}
                           ,{'docs', NewDocs}
@@ -299,8 +299,8 @@ generate_row([?VARS], Props) ->
              ,{<<"routes">>, [<<"^\\+?", Prefix/binary, ".+$">>]}
               %% override account-ID from task props
              ,{<<"account_id">>, props:get_value('account_id', Props)}
-             ,{<<"pvt_auth_account_id">>, props:get_value('auth_account_id', Props)}
-             ],
+                                ,{<<"pvt_auth_account_id">>, props:get_value('auth_account_id', Props)}
+                                ],
     kz_json:from_list(props:filter_undefined(props:set_values(Update, List))).
 
 -spec save_rates(ne_binary(), kz_json:objects()) -> 'ok'.
